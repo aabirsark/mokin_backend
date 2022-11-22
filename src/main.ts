d613f2b230b04300a043ec8ad8e1ca757bea2a6a
aabirsark <factbomberz.1@gmail.com>
2022-11-23 01:11:31 +0530

    update

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService: ConfigService = new ConfigService();
-  await app.listen(configService.get('PORT') || '8080');
+  app.enableCors();
+  await app.listen(configService.get('PORT') || '3000');
 }
 bootstrap();
